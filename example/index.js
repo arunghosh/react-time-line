af194be7cf84d7ddfa17b33fa27e00edc3bafa51
Arun Ghosh <arunghosh@gmail.com>
2017-09-17 10:48:36 +0530

    Fixed issue with style

--- example/index.js
+++ example/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Timeline from '../src';
 
-ReactDOM.render(
-  <Timeline items={[ { ts: new Date().toISOString(), text: 'Something happened' } ]} />,
-  document.getElementById('app')
-);
+const events = [
+  {ts: new Date().toISOString(), text: 'Logged in'},
+  {ts: new Date().toISOString(), text: 'Clicked Home Page'},
+  {ts: new Date().toISOString(), text: 'Edited Profile'},
+];
 
-
+ReactDOM.render(<Timeline items={events} />, document.getElementById('app'));
