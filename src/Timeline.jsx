f63a206911f7e8e2512110726ff225585a4a2bbf
Arun Ghosh <arunghosh@gmail.com>
2018-12-01 16:01:39 +0530

    formatted code

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -1,11 +1,10 @@
-import moment from 'moment';
-import React from 'react';
-import PropTypes from 'prop-types';
-import TimlineItem from './TimlineItem';
-
+import moment from "moment";
+import React from "react";
+import PropTypes from "prop-types";
+import TimlineItem from "./TimlineItem";
 
 /**
- * Converts array of events in to object having date as the key and list of 
+ * Converts array of events in to object having date as the key and list of
  * event for that date as the value
  *
  * @param {Array} items Array of events in the form of ts and text
@@ -13,12 +12,12 @@ import TimlineItem from './TimlineItem';
  */
 function getFormattedData(items) {
   const activities = {};
-  items.forEach(({ts, text}, index) => {
+  items.forEach(({ ts, text }, index) => {
     const date = moment(ts);
-    const dateStr = date.format('DD MMM YYYY');
+    const dateStr = date.format("DD MMM YYYY");
     const list = activities[dateStr] || [];
     list.push({
-      time: date.format('hh:mm'),
+      time: date.format("hh:mm"),
       text,
       key: index,
     });
@@ -27,32 +26,32 @@ function getFormattedData(items) {
   return activities;
 }
 
-function Timeline({items}) {
+function Timeline({ items }) {
   const activities = getFormattedData(items);
   const dates = Object.keys(activities);
   return (
     <div className="time-line-ctnr">
-      {dates.map(d =>
+      {dates.map(d => (
         <ul className="time-line" key={d}>
           <li className="time-label">
-            <span>
-              {d}
-            </span>
+            <span>{d}</span>
           </li>
-          {activities[d].map(({time, text, key}) =>
-            <TimlineItem time={time} text={text} key={key} />,
-          )}
-        </ul>,
-      )}
+          {activities[d].map(({ time, text, key }) => (
+            <TimlineItem time={time} text={text} key={key} />
+          ))}
+        </ul>
+      ))}
     </div>
   );
 }
 
 Timeline.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.shape({
-    ts: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-  })).isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      ts: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Timeline;
--- src/TimlineItem.jsx
+++ src/TimlineItem.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 /**
- * @usage 
+ * @usage
  * <TimlineItem time={time} text={text} />
  */
-function TimlineItem({time, text}) {
+function TimlineItem({ time, text }) {
   return (
     <li>
       <i className="fa" />
       <div className="time-line-item">
         <span className="time">
-          <i className="fa fa-clock-o" /> {time}
+          <i className="fa fa-clock-o" />
+          {time}
         </span>
-        <div className="time-line-header">
-          {text}
-        </div>
+        <div className="time-line-header">{text}</div>
       </div>
     </li>
   );
